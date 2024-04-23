851d5f062d279153193a6d73053811b7a5587ae9
SabihAli <sabih.ali.9000@gmail.com>
2024-04-23 19:01:26 +0500

    hehe

---
+++ app/src/main/java/com/nustfruta/CartAndCheckout/CartActivity.java
@@ -0,0 +1,18 @@
+package com.nustfruta.CartAndCheckout;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.nustfruta.R;
+
+public class CartActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.cart_activity);
+
+
+    }
+}
