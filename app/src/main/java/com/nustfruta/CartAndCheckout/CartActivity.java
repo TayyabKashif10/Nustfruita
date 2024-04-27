8d0a65ac3abe630dcd611952256ee307a33113f4
SabihAli <sabih.ali.9000@gmail.com>
2024-04-27 19:58:12 +0500

    e

--- app/src/main/java/com/nustfruta/CartAndCheckout/CartActivity.java
+++ app/src/main/java/com/nustfruta/CartAndCheckout/CartActivity.java
@@ -26,7 +26,6 @@ public class CartActivity extends AppCompatActivity {
     public static RecyclerViewAdapter recyclerViewAdapter;
 
 
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,7 +64,6 @@ public class CartActivity extends AppCompatActivity {
     }
 
 
-
     static class RecyclerViewAdapter extends RecyclerView.Adapter<com.nustfruta.CartAndCheckout.CartActivity.ViewHolder> {
 
 
@@ -82,13 +80,11 @@ public class CartActivity extends AppCompatActivity {
         // Where to get the single card as view holder object
         @NonNull
         @Override
-        public CartActivity.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
-        {
+        public CartActivity.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
             LayoutInflater inflater = LayoutInflater.from(context);
 
-            if(viewType == 1)
-            {
+            if (viewType == 1) {
                 View view = inflater.inflate(R.layout.explore_menu_row, parent, false);
                 viewHolder = new CartActivity.ViewHolder(view, viewType);
                 return viewHolder;
@@ -104,14 +100,13 @@ public class CartActivity extends AppCompatActivity {
         @Override
         public void onBindViewHolder(@NonNull CartActivity.ViewHolder holder, int position) {
 
-                if(getItemViewType(position) == 0)
-                {
-                    Product product = productArrayList.get(position);
-                    holder.productName.setText(product.getName());
-                    holder.price.setText(product.getProductPrice());
-                    holder.quantity.setText(product.getStringQuantity());
-                    holder.productIcon.setImageResource(productArrayList.get(position).getImage());
-                }
+            if (getItemViewType(position) == 0) {
+                Product product = productArrayList.get(position);
+                holder.productName.setText(product.getName());
+                holder.price.setText(product.getProductPrice());
+                holder.quantity.setText(product.getStringQuantity());
+                holder.productIcon.setImageResource(productArrayList.get(position).getImage());
+            }
         }
 
         // How many items
@@ -122,7 +117,7 @@ public class CartActivity extends AppCompatActivity {
 
         @Override
         public int getItemViewType(int position) {
-            if(position == productArrayList.size())
+            if (position == productArrayList.size())
                 return 1;
             else
                 return 0;
@@ -146,30 +141,29 @@ public class CartActivity extends AppCompatActivity {
             minusButton = itemView.findViewById(R.id.minusButton);
 
             plusButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                minusButton.setImageResource(R.drawable.minus_icon);
-                CartActivity.productArrayList.get(getAdapterPosition()).increaseQuantity();
-                CartActivity.recyclerViewAdapter.notifyItemChanged(getAdapterPosition());
-            }
+                @Override
+                public void onClick(View v) {
+                    minusButton.setImageResource(R.drawable.minus_icon);
+                    CartActivity.productArrayList.get(getAdapterPosition()).increaseQuantity();
+                    CartActivity.recyclerViewAdapter.notifyItemChanged(getAdapterPosition());
+                }
             });
 
             minusButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                CartActivity.productArrayList.get(getAdapterPosition()).decreaseQuantity();
+                    CartActivity.productArrayList.get(getAdapterPosition()).decreaseQuantity();
 
-                if (CartActivity.productArrayList.get(getAdapterPosition()).quantity == 0) {
-                    CartActivity.productArrayList.remove(getAdapterPosition());
-                    CartActivity.recyclerViewAdapter.notifyItemRemoved(getAdapterPosition());
-                } else
-                    CartActivity.recyclerViewAdapter.notifyItemChanged(getAdapterPosition());
+                    if (CartActivity.productArrayList.get(getAdapterPosition()).quantity == 0) {
+                        CartActivity.productArrayList.remove(getAdapterPosition());
+                        CartActivity.recyclerViewAdapter.notifyItemRemoved(getAdapterPosition());
+                    } else
+                        CartActivity.recyclerViewAdapter.notifyItemChanged(getAdapterPosition());
 
 
                 }
-                });
-            }
-
+            });
+        }
 
 
         public ViewHolder(@NonNull View itemView, int itemViewType) {
