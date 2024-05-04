11b903335bca401e001dd93ac9ce9302aad7eaa7
Tayyab Kashif <149696917+TayyabKashif10@users.noreply.github.com>
2024-05-04 06:41:12 +0500

    made a menu ayeee

--- app/src/main/java/com/nustfruta/authentication/LoginPhoneNumberActivity.java
+++ app/src/main/java/com/nustfruta/authentication/LoginPhoneNumberActivity.java
@@ -45,7 +45,7 @@ public class LoginPhoneNumberActivity extends AppCompatActivity implements View.
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login_phone_number);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawerLayout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
--- app/src/main/java/com/nustfruta/dashboard/MenuActivity.java
+++ app/src/main/java/com/nustfruta/dashboard/MenuActivity.java
@@ -2,12 +2,15 @@ package com.nustfruta.dashboard;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewPropertyAnimator;
-import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -16,8 +19,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -25,6 +30,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
 import com.nustfruta.R;
+import com.nustfruta.authentication.ProfileActivity;
 import com.nustfruta.utility.Constants;
 import com.nustfruta.utility.FirebaseUtil;
 
@@ -35,7 +41,21 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
 
+        if (v.getId()==optionsIcon.getId())
+        {
+            openDrawer();
+        } else if (v.getId()==profile.getId()) {
+            navigateOut(ProfileActivity.class);
+        }
+
+        //TODO: navigate out for other buttons.
+
     }
+
+    DrawerLayout drawerLayout;
+    LinearLayout orders, profile, about, logout;
+
+    ImageView optionsIcon;
 
     // TODO: remove this, should always be enabled
     boolean factChangeEnabled = false;
@@ -49,12 +69,12 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
     CardView cardA, cardB, currentCard;
     TextView textA, textB;
 
-    Toolbar toolbar;
-
     // number of fruit facts stored in database
     int fruitFactNumber;
 
     String fetchedFact;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,35 +82,71 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
 
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_menu);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawerLayout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
         fetchedFact = getString(R.string.sample_fact);
         initializeViews();
-        setSupportActionBar(toolbar);
+        attachListeners();
         syncFruitFactNumber();
         factChangeHandler = new Handler();
     }
     @Override
     protected void onResume() {
 
+        /*
+        * the drawer is left open when user navigates to another activity (this activity is set to pause) so if its open when user comes back, close it.
+        * i found this to be a better placement then putting it in pause, because in pause the system first waits for the drawer to close, which is jittery
+        * in on resume its not jittery at all.
+        * */
+        closeDrawer();
+        super.onResume();
         factChangeHandler.postDelayed( factChangeRunnable = new Runnable() {
             public void run() {
                 changeFact();
                 factChangeHandler.postDelayed(factChangeRunnable, Constants.FACT_CHANGE_DELAY);
             }
         }, Constants.FACT_CHANGE_DELAY);
-
-        super.onResume();
     }
     @Override
     protected void onPause() {
-
+        super.onPause();
         // stop handler when activity is paused.
         factChangeHandler.removeCallbacks(factChangeRunnable);
-        super.onPause();
+
+    }
+
+    public void openDrawer()
+    {
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    public void closeDrawer()
+    {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+
+    public void navigateOut(Class activity)
+    {
+        Intent intent = new Intent(MenuActivity.this, activity);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+
+    }
+
+
+    public void attachListeners()
+    {
+        optionsIcon.setOnClickListener(this);
+        orders.setOnClickListener(this);
+        profile.setOnClickListener(this);
+        about.setOnClickListener(this);
+        logout.setOnClickListener(this);
     }
 
     public void syncFruitFactNumber()
@@ -113,12 +169,15 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
 
     public void initializeViews()
     {
-        toolbar = findViewById(R.id.toolBar);
+        optionsIcon = findViewById(R.id.hamburgerIcon);
+        drawerLayout = findViewById(R.id.drawerLayout);
         cardA = findViewById(R.id.cardA);
         currentCard = cardA;
         cardB = findViewById(R.id.cardB);
         textA = findViewById(R.id.textA);
         textB = findViewById(R.id.textB);
+        orders=findViewById(R.id.orderRow); profile=findViewById(R.id.profileRow);
+        about=findViewById(R.id.aboutRow); logout=findViewById(R.id.logoutRow);
     }
 
     public void setFruitFact(TextView v)
@@ -137,7 +196,6 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
     }
-
 
     public void changeFact() {
 
